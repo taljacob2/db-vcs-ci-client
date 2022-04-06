9994c2e1eeb540af401a88b12dfac59cb4f14509
Tal Jacob <taljacob2@gmail.com>
2022-04-06 12:43:01 +0300

    Fix `export-db.sh` name
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-LOG_TITLE="##export-db.sh##: "
+LOG_TITLE="### export-db.sh ###: "
 
 echo "#######################"
 echo $LOG_TITLE "Exporting DB..."
-curl -d @export-db.sql https://localhost:7179/api/upload-export-db-sql
-echo $LOG_TITLE "Export Process Finished."
+curl -d @git-config-setup/db-vcs-ci/export-db/export-db.sql https://localhost:7179/api/upload-export-db-sql
+echo $LOG_TITLE "Export DB Process Finished."
 echo "#######################"
