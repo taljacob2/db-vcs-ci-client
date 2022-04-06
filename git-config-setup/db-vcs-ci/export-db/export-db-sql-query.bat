0459c4b7d20f59b5756e30639107e79984a5045c
Tal Jacob <taljacob2@gmail.com>
2022-04-06 17:35:48 +0300

    test
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
+++ git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
@@ -1,1 +1,2 @@
-mkdir C:\Bak && sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
+mkdir C:\Bak && 
+sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'" && 
