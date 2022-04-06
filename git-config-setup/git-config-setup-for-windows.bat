6558285753cae63d8bbc246ebd95e906a20bd95c
Tal Jacob <taljacob2@gmail.com>
2022-04-06 11:26:17 +0300

    Update git-config-setups to support `pre-commit.sh`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/git-config-setup-for-windows.bat
+++ git-config-setup/git-config-setup-for-windows.bat
@@ -1,2 +1,4 @@
 git config merge.keepTheirs.driver "./git-config-setup/merge-driver/keepTheirs.sh %O %A %B"
 git config merge.keepMine.driver "./git-config-setup/merge-driver/keepMine.sh %O %A %B"
+
+copy git-config-setup/db-ci/pre-commit.sh .git/hooks/pre-commit
