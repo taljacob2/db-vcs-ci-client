13b701b06ecc3eec4c7f18f457514f441ed3e8d8
Tal Jacob <taljacob2@gmail.com>
2022-04-06 12:50:54 +0300

    Renamed api to export the db
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -4,6 +4,6 @@ LOG_TITLE="### export-db.sh ###: "
 
 echo "#######################"
 echo $LOG_TITLE "Exporting DB..."
-curl -d @git-config-setup/db-vcs-ci/export-db/export-db.sql https://localhost:7179/api/upload-export-db-sql
+curl -d @git-config-setup/db-vcs-ci/export-db/export-db.sql https://localhost:7179/api/execute-a-export-db-sql
 echo $LOG_TITLE "Export DB Process Finished."
 echo "#######################"
