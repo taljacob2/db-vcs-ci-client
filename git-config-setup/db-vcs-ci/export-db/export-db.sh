bdc349af35c8d534f5456e5077b62a8f01c36837
Tal Jacob <taljacob2@gmail.com>
2022-04-06 13:15:21 +0300

    Posting `export-db.sql`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -7,8 +7,7 @@ echo
 echo $LOG_BOUNDARY
 echo $LOG_TITLE "Exporting DB..."
 echo
-# curl -k -d @git-config-setup/db-vcs-ci/export-db/export-db.sql https://localhost:7179/api/execute-a-export-db-sql
-curl -k https://localhost:7179/api/script
+curl -k -d @git-config-setup/db-vcs-ci/export-db/export-db.sql https://localhost:7179/api/execute-a-export-db-sql
 echo
 echo $LOG_TITLE "Export DB Process Finished."
 echo $LOG_BOUNDARY
