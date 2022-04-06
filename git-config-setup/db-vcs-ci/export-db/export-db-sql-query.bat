f690a92f41ea0b9eefa01f3333b9d14179e1cb0e
Tal Jacob <taljacob2@gmail.com>
2022-04-06 18:12:49 +0300

    Update move `&`

--- git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
+++ git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
@@ -1,4 +1,4 @@
-mkdir C:\Bak && 
+mkdir C:\Bak &
 sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'" && 
 move C:\Bak\db.bak C:\Theoretical-Server-Backend\db.bak && 
 PAUSE
