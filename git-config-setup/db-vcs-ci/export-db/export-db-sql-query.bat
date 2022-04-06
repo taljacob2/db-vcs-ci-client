89a201530c6e4384dedce685db05a596189d66fc
Tal Jacob <taljacob2@gmail.com>
2022-04-06 17:23:30 +0300

    Add `mkdir` to make sure `sqlcmd` always works on server
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
+++ git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat
@@ -1,1 +1,1 @@
-sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Code\db-vcs-ci-client\git-config-setup\db-vcs-ci\db.bak'"
+mkdir C:\Bak && sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
