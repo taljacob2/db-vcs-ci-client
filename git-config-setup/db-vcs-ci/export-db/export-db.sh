5d5e23a16aba24843466c0189565f80a9e9e3396
taljacob2 <taljacob2@gmail.com>
2022-04-07 15:35:23 +0300

    Extract httpstatus
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -31,19 +31,22 @@ echo
 
 URL="$SERVER/api/execute-cmd-command?workingDirectory=$WORKING_DIRECTORY_IN_SERVER"
 
-TEST=$(curl -k -X 'POST' \
+HTTP_RESPONSE=$(curl -k -X 'POST' \
               $URL \
               -H 'accept: */*' \
               -H 'Content-Type: application/sql' \
               -w %{http_code} \
               -d @git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat)
 
+HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -E 's/.*([0-9]{3})$/\1/')
+
 echo
-echo $TEST
+echo $HTTP_STATUS
 echo "----------------------------------------"
-echo $TEST
+echo $HTTP_STATUS
+echo $HTTP_STATUS
 echo "----------------------------------------"
-echo $TEST
+echo $HTTP_STATUS
 
 if [[ "$status_code" -ne 200 ]] ; then
     echo $LOG_TITLE "Encountered An Error."
