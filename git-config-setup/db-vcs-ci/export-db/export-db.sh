84435964abc648362caac45d791880ac005c1795
taljacob2 <taljacob2@gmail.com>
2022-04-07 08:57:47 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -9,6 +9,8 @@ echo $LOG_TITLE "Attempting To Export DB From Server..."
 
 echo $LOG_TITLE "Attempting To Create .bak File In The Server..."
 
+echo $LOG_TITLE "SHOWING OUTPUT FROM SERVER:"
+
 echo
 curl -k -X 'POST' \
             'https://localhost:7179/api/execute-cmd-command?workingDirectory=C:\Bak' \
@@ -18,7 +20,7 @@ curl -k -X 'POST' \
 echo
 
 
-echo
+echo $LOG_TITLE "DONE SHOWING OUTPUT FROM SERVER."
 echo $LOG_TITLE "Export DB Process Finished."
 echo $LOG_BOUNDARY
 echo
