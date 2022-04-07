8fcae0f468a054998359e699870b33a1b3d35eeb
taljacob2 <taljacob2@gmail.com>
2022-04-07 06:51:10 +0300

    Split `export-db.sh` curl to two requests
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -6,22 +6,39 @@ LOG_BOUNDARY="#######################"
 echo
 echo $LOG_BOUNDARY
 echo $LOG_TITLE "Attempting To Export DB From Server..."
-echo
 
+echo $LOG_TITLE "Attempting To Create .bak File In The Server..."
+
+echo
 status_code=$(curl -k -X 'POST' \
               'https://localhost:7179/api/execute-a-export-db-sql?pathToExportBakInServer=C:\Bak\db.bak' \
               -H 'accept: */*' \
               -H 'Content-Type: application/sql' \
               -d @git-config-setup/db-vcs-ci/export-db/export-db-sql-query.bat \
-              --output git-config-setup/db-vcs-ci/db.bak \
               --write-out %{http_code})
 echo
 
 if [[ "$status_code" -ne 200 ]] ; then
   echo $LOG_TITLE "Encountered An Error."
 else
-    echo $LOG_TITLE "Received .bak File Successfully!"
+    echo $LOG_TITLE "Successfully Created .bak File In The Server!"
+    echo $LOG_TITLE "Attempting To Download .bak File From Server..."
+
+    echo
+    status_code=$(curl -k -X 'GET' \
+                  -H 'accept: */*' \
+                  'https://localhost:7179/api/download-file?filePathInServer=C:\Bak\db.bak&mimeType=application/octet-stream' \
+                  --output git-config-setup/db-vcs-ci/db.bak \
+                  --write-out %{http_code})
+    echo
+
+    if [[ "$status_code" -ne 200 ]] ; then
+      echo $LOG_TITLE "Encountered An Error."
+    else
+        echo $LOG_TITLE "Received .bak File Successfully!"
+    fi
 fi
+
 
 echo
 echo $LOG_TITLE "Export DB Process Finished."
