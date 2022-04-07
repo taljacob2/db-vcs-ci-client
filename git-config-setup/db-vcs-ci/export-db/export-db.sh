8ac68739b81d1c5fda80c17ed59cc6f10633f472
taljacob2 <taljacob2@gmail.com>
2022-04-07 13:34:46 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- git-config-setup/db-vcs-ci/export-db/export-db.sh
+++ git-config-setup/db-vcs-ci/export-db/export-db.sh
@@ -14,9 +14,9 @@ LOG_BOUNDARY="################################"
 SERVER_LOG_HALF_BOUNDARY="###########"
 
 URL="$SERVER/api/execute-cmd-command?workingDirectory=$WORKING_DIRECTORY"
-URL="$URL"
+URL=\'"$URL"\'
 
-echo \'"$URL"\'
+echo $URL
 
 echo
 echo $LOG_BOUNDARY
