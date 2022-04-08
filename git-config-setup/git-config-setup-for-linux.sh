e5fdd9cacb4b47a060e3cc4ca181c5ce985f31d5
taljacob2 <taljacob2@gmail.com>
2022-04-08 14:53:01 +0300

    Made new config setup to linux installation
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- git-config-setup/git-config-setup-for-linux.sh
+++ git-config-setup/git-config-setup-for-linux.sh
@@ -1,5 +1,5 @@
 git config merge.keepTheirs.driver "./git-config-setup/merge-driver/keepTheirs.sh %O %A %B"
 git config merge.keepMine.driver "./git-config-setup/merge-driver/keepMine.sh %O %A %B"
 
-ln -s git-config-setup/db-vcs-ci/export-db/pre-commit.sh .git/hooks/pre-commit
-ln -s git-config-setup/db-vcs-ci/export-db/post-commit.sh .git/hooks/post-commit
+ln -s git-config-setup/db-vcs-ci/config/pre-commit.sh .git/hooks/pre-commit
+ln -s git-config-setup/db-vcs-ci/config/post-commit.sh .git/hooks/post-commit
