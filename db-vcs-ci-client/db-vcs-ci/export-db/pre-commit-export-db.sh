061e85a6994c9b7608cd3b4e45ccdd44d3dddbce
taljacob2 <taljacob2@gmail.com>
2022-04-08 17:12:17 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/pre-commit-export-db.sh
+++ db-vcs-ci-client/db-vcs-ci/export-db/pre-commit-export-db.sh
@@ -5,8 +5,8 @@ source db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-setup.sh
 # ---------------------------------- Code -------------------------------------
 
 LOG_TITLE="### export-db.sh ###: "
-LOG_BOUNDARY="################################"
-SERVER_LOG_HALF_BOUNDARY="###########"
+LOG_BOUNDARY="###############################################################################"
+SERVER_LOG_HALF_BOUNDARY="#########################"
 
 echo
 echo $LOG_BOUNDARY
@@ -14,7 +14,7 @@ echo $LOG_TITLE "Attempting To Export DB From Server..."
 
 echo $LOG_TITLE "Attempting To Create .bak File In The Server..."
 
-echo $LOG_TITLE $SERVER_LOG_HALF_BOUNDARY OPENED OUTPUT FROM SERVER $SERVER_LOG_HALF_BOUNDARY
+echo $SERVER_LOG_HALF_BOUNDARY OPENED OUTPUT FROM SERVER $SERVER_LOG_HALF_BOUNDARY
 
 echo
 
@@ -75,7 +75,7 @@ else
     fi
 fi
 
-echo $LOG_TITLE $SERVER_LOG_HALF_BOUNDARY CLOSED OUTPUT FROM SERVER $SERVER_LOG_HALF_BOUNDARY
+echo $SERVER_LOG_HALF_BOUNDARY CLOSED OUTPUT FROM SERVER $SERVER_LOG_HALF_BOUNDARY
 echo
 echo $LOG_TITLE "Export DB Process Finished."
 echo $LOG_BOUNDARY
