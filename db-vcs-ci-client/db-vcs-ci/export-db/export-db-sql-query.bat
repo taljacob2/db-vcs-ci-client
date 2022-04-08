ed520623ea64ad42698e4b91f156cb70a095d135
taljacob2 <taljacob2@gmail.com>
2022-04-08 18:57:19 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/export-db-sql-query.bat
+++ db-vcs-ci-client/db-vcs-ci/export-db/export-db-sql-query.bat
@@ -1,1 +1,1 @@
-sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
+rm C:\Bak\db.bak && sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
