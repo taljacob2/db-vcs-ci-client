170d46c52b8ccec904c2cd635781689a699f105b
taljacob2 <taljacob2@gmail.com>
2022-04-08 20:53:54 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/post-commit-export-db.sh
+++ db-vcs-ci-client/db-vcs-ci/export-db/post-commit-export-db.sh
@@ -1,31 +1,41 @@
-# #!/bin/sh
+#!/bin/sh
 
-# source db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-intern-config.sh
+source db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-intern-config.sh
 
-# # ---------------------------------- Code -------------------------------------
+# ---------------------------------- Code -------------------------------------
 
-# LOG_TITLE="### post-commit-export-db.sh ###: "
+LOG_TITLE="### post-commit-export-db.sh ###: "
 
-# echo
+echo
 
-# # In case there is an on-going execution to export the db.
-# if [ -e $EXPORT_DB_FOLDER_PATH/.commit ]
-#     then
-#     rm $EXPORT_DB_FOLDER_PATH/.commit
+# In case there is an on-going execution to export the db.
+if [ -e $EXPORT_DB_FOLDER_PATH/.commit ]
+    then
+    rm $EXPORT_DB_FOLDER_PATH/.commit
 
-#     echo $LOG_TITLE "Adding The '.bak' File To Your Commit..."
 
-#     RESULT=$?
-#     git add $EXPORTED_DB_BAK_PATH_IN_CLIENT
-#     if [ $RESULT -ne 0 ]
-#         then
-#         echo $ERROR_MESSAGE
-#     else        
-#         echo $LOG_TITLE "Successfully Added The '.bak' File To Your Commit!"
-#         git commit --amend -C HEAD --no-verify
-#     fi
-# fi
+    # Get to code.  Exit if unsaved changes in repo
+    if `git status db-vcs-ci-client/db-vcs-ci/db.bak | grep -q "nothing to commit"`; then
+        echo GOOD
+    else
+        echo BAD
+    fi
 
-# # echo $LOG_BOUNDARY
 
-# exit
+
+    # echo $LOG_TITLE "Adding The '.bak' File To Your Commit..."
+
+    # RESULT=$?
+    # git add $EXPORTED_DB_BAK_PATH_IN_CLIENT
+    # if [ $RESULT -ne 0 ]
+    #     then
+    #     echo $ERROR_MESSAGE
+    # else        
+    #     echo $LOG_TITLE "Successfully Added The '.bak' File To Your Commit!"
+    #     git commit --amend -C HEAD --no-verify
+    # fi
+fi
+
+# echo $LOG_BOUNDARY
+
+exit
