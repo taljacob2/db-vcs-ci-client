81db9d5f6093467b3cda0ee4f10ceee165ef1b46
taljacob2 <taljacob2@gmail.com>
2022-04-09 12:57:48 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/cmd-command-for-windows-server.bat
+++ db-vcs-ci-client/db-vcs-ci/export-db/cmd-command-for-windows-server.bat
@@ -1,1 +1,2 @@
-del /f C:\Bak\db.bak && sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
+del /f C:\Bak\db.bak
+sqlcmd -S (localdb)\Local -Q "BACKUP DATABASE [Klil-Local-Tal] TO DISK = 'C:\Bak\db.bak'"
