e5049dba4fab457171f2b6f8d6645112937a52a4
taljacob2 <taljacob2@gmail.com>
2022-04-09 17:44:38 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -9,6 +9,6 @@ EXPORTED_DB_BAK_NAME_IN_CLIENT="db.bak"
 
 # ------------------------ Server Advanced Settings ---------------------------
 
-WORKING_DIRECTORY_IN_SERVER="C:\Bak"
+WORKING_DIRECTORY_IN_SERVER="C:\Bak Two"
 EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="db.bak"
 SERVER="https://localhost:7179"
