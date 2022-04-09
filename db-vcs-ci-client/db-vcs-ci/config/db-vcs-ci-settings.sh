386a404e3a44f7053608b0966aa1be4032c45ecb
taljacob2 <taljacob2@gmail.com>
2022-04-09 18:01:44 +0300

    Update `db-vcs-ci-settings.sh` doc
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# WARNING: Do not choose paths or names that contain white-spaces.
+
 # ---------------------------- Basic Settings ---------------------------------
 
 COMPUTER_NAME="(localdb)"
@@ -9,6 +11,6 @@ EXPORTED_DB_BAK_NAME_IN_CLIENT="db.bak"
 
 # ------------------------ Server Advanced Settings ---------------------------
 
-WORKING_DIRECTORY_IN_SERVER="C:\Bak" # WARNING: Do not choose a path that contains white-spaces in it.
+WORKING_DIRECTORY_IN_SERVER="C:\Bak"
 EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="db.bak"
 SERVER="https://localhost:7179"
