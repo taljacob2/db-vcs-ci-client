c9927ce6e734807282110cacee217b2165833a06
Tal Jacob <taljacob2@gmail.com>
2022-04-10 17:28:08 +0300

    test
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/cmd-command-for-windows-server.bat
+++ db-vcs-ci-client/db-vcs-ci/export-db/cmd-command-for-windows-server.bat
@@ -1,2 +1,2 @@
 del /f %3\%4 >nul 2>&1
-runas /user:WBX-VPS-P42\trustech sqlcmd -S %1 -Q "BACKUP DATABASE [%2] TO DISK = '%3\%4'"
+runas /user:WBX-VPS-P42\trustech sqlcmd.exe -S %1 -Q "BACKUP DATABASE [%2] TO DISK = '%3\%4'"
