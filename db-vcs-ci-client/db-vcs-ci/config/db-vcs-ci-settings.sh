c48616aafe7950faf78a20d0ac41042d1484d0a3
taljacob2 <taljacob2@gmail.com>
2022-04-11 07:08:31 +0300

    Edited docs in `db-vcs-ci-settings.sh`
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -4,28 +4,40 @@
 
 # ---------------------------- Basic Settings ---------------------------------
 
-# For example: `"(localdb)\\Local"``
+: '
+For example:
+
+- "(localdb)\\Local"
+- "(localdb)\\MSSQLDBLocal"
+- localhost:
+  - "."
+  - ".\\"
+  - "localhost"
+  - "localhost\\"
+'
 COMPUTER_NAME_SLASH_INSTANCE_NAME="localhost"
+
 DB_NAME="KlilDBCore"
+
 EXPORTED_DB_BAK_NAME_IN_CLIENT="db.bak"
 
 # ------------------------ Server Advanced Settings ---------------------------
 
 WORKING_DIRECTORY_IN_SERVER="C:\Bak"
 
-# If there are many "db-vcs-ci-clients" that use this database,
-# it is recommended to choose a 
-
-"""
-sdasda
-asdsad
-"""
-
 : '
+If there are many "db-vcs-ci-clients" that use this database,
+and they all have the same `WORKING_DIRECTORY_IN_SERVER` name,
+it is recommended (but not mandatory) to choose a unique name for
+`EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY`, so in the working-directory
+of the server the backups of all the contributors that use it will be presented.
 
-56767
-567
-567567
+For example:
+
+- "taljacob-db.bak"
+- "johndoe-db.bak"
+- "richardroe-db.bak"
 '
-EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="db.bak"
+EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="taljacob-db.bak"
+
 SERVER="https://dbvcsci.trustech.co.il"
