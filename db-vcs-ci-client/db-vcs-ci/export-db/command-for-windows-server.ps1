ef83d7c4bac0f7e21e3799b0dae62d1f1c3123ac
taljacob2 <taljacob2@gmail.com>
2022-04-11 11:04:21 +0300

    test2
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/command-for-windows-server.ps1
+++ db-vcs-ci-client/db-vcs-ci/export-db/command-for-windows-server.ps1
@@ -1,7 +1,7 @@
 # ------------------------------- Credentials  --------------------------------
 
-$pass = ConvertTo-SecureString $args[4] -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($args[5], $pass)
+$pass = ConvertTo-SecureString $args[5] -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($args[4], $pass)
 Get-Credential -Credential $credential
 
 # ---------------------------------- Code -------------------------------------
