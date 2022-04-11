4059419e534e21be21fec1d3604c1b9f80cc348e
taljacob2 <taljacob2@gmail.com>
2022-04-11 10:56:49 +0300

    test2
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -43,6 +43,6 @@ EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="taljacob-db.bak"
 # SERVER="https://dbvcsci.trustech.co.il"
 SERVER="https://localhost:7179"
 
-USERNAME="tal"
+USERNAME="tal129387"
 
 PASSWORD="129729tal"
