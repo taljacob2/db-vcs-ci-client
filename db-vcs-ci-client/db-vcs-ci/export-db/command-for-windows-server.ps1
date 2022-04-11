f6b62333c42030cff1cc8f1e37b459e4755c0c33
Tal Jacob <taljacob2@gmail.com>
2022-04-11 12:34:41 +0300

    test
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/export-db/command-for-windows-server.ps1
+++ db-vcs-ci-client/db-vcs-ci/export-db/command-for-windows-server.ps1
@@ -8,5 +8,5 @@ Get-Credential -Credential $credential
 
 echo "-------------------------------------"
 
-Remove-Item  C:\Bak\taljacob-db.bak >nul 2>&1
+Remove-Item  C:\Bak\taljacob-db.bak | Out-Null
 # sqlcmd -S $args[0] -U db-vcs-ci -P Trust1234! -Q "BACKUP DATABASE [$args[1]] TO DISK = '$args[2]\$args[3]'"
