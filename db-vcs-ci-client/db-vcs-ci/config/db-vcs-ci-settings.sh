9d16ee7bd9d234aa180509858862d8a1139b9f9d
taljacob2 <taljacob2@gmail.com>
2022-04-11 06:50:09 +0300

    test
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -4,12 +4,28 @@
 
 # ---------------------------- Basic Settings ---------------------------------
 
-COMPUTER_NAME_SLASH_INSTANCE_NAME="localhost"  # For example: `"(localdb)\\Local"``
+# For example: `"(localdb)\\Local"``
+COMPUTER_NAME_SLASH_INSTANCE_NAME="localhost"
 DB_NAME="KlilDBCore"
 EXPORTED_DB_BAK_NAME_IN_CLIENT="db.bak"
 
 # ------------------------ Server Advanced Settings ---------------------------
 
 WORKING_DIRECTORY_IN_SERVER="C:\Bak"
+
+# If there are many "db-vcs-ci-clients" that use this database,
+# it is recommended to choose a 
+
+"""
+sdasda
+asdsad
+"""
+
+: '
+
+56767
+567
+567567
+'
 EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="db.bak"
 SERVER="https://dbvcsci.trustech.co.il"
