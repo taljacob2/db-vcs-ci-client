d78c3e0902d879624217a40a9283f0a722bfb468
Tal Jacob <taljacob2@gmail.com>
2022-04-11 12:30:52 +0300

    test
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
+++ db-vcs-ci-client/db-vcs-ci/config/db-vcs-ci-settings.sh
@@ -40,8 +40,8 @@ For example:
 '
 EXPORTED_DB_BAK_NAME_IN_SERVER_WORKING_DIRECTORY="taljacob-db.bak"
 
-SERVER="https://dbvcsci.trustech.co.il"
-# SERVER="https://localhost:7179"
+# SERVER="https://dbvcsci.trustech.co.il"
+SERVER="https://localhost:7179"
 
 USERNAME="tal"
 
